090948ae41e9fd85ccb5ce9817b36c42291da66c
pranav04katiyar <pranav04katiyar@gmail.com>
2024-04-09 21:43:52 +0530

    Added Product and category model classes

---
+++ src/main/java/com/project/ecommerce_productservice/models/Category.java
@@ -0,0 +1,6 @@
+package com.project.ecommerce_productservice.models;
+
+public class Category {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/project/ecommerce_productservice/models/Product.java
@@ -0,0 +1,10 @@
+package com.project.ecommerce_productservice.models;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private Category category;
+}
