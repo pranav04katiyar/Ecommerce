0e6d8dbcd951667a49a054eddc11e4003537694f
pranav04katiyar <pranav04katiyar@gmail.com>
2024-04-10 10:08:40 +0530

    Updated Category model class

--- src/main/java/com/project/ecommerce_productservice/models/Category.java
+++ src/main/java/com/project/ecommerce_productservice/models/Category.java
@@ -1,5 +1,10 @@
 package com.project.ecommerce_productservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Category {
     private Long id;
     private String name;
